b914c9dcefa8fe394d6b12f2bab7dfc9506774b1
SlackinJack <41076523+SlackinJack@users.noreply.github.com>
2025-01-12 04:38:23 -0700

    add upscale sampler, overhaul pipeline loader

--- nodes.py
+++ nodes.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python3
 import base64
 import json
-import numpy
 import os
 import pickle
 import requests
 import subprocess
 import time
 import torch
-import torchvision.transforms.functional as F
 from pathlib import Path
-from PIL import Image, ImageSequence
-from torchvision.io import decode_image
+from PIL import Image
 from torchvision.transforms import ToPILImage, ToTensor
 
 
@@ -20,38 +17,21 @@ comfy_root = os.path.dirname(os.path.dirname(cwd))
 models_dir = os.path.join(os.path.join(comfy_root, "models"))
 checkpoints_dir = os.path.join(models_dir, "checkpoints")
 outputs_dir = os.path.join(comfy_root, "output")
-
 models = [d for d in os.listdir(checkpoints_dir) if os.path.isdir(os.path.join(checkpoints_dir, d))]
 
 
-pipeline_process = None
+host_address = 'http://localhost:6000'
+host_process = None
+host_address_generate = f'{host_address}/generate'
+host_address_initialize = f'{host_address}/initialize'
 
 
-class AsyncDiffSVDPipelineLoader:
+class ADPipelineConfig:
     @classmethod
     def INPUT_TYPES(s):
         return {
             "required": {
                 "model": (models,),
-                # "scheduler": (
-                #     list([
-                #         "ddim",
-                #         "euler",
-                #         "euler_a",
-                #         "dpm_2",
-                #         "dpm_2_a",
-                #         "dpmpp_2m",
-                #         "dpmpp_2m_sde",
-                #         "dpmpp_sde",
-                #         "heun",
-                #         "lms",
-                #         "pndm",
-                #         "unipc",
-                #     ]),
-                #     {
-                #         "default": "dpmpp_2m",
-                #     }
-                # ),
                 "nproc_per_node": (
                     "INT",
                     {
@@ -96,57 +76,93 @@ class AsyncDiffSVDPipelineLoader:
                         "default": "fp16",
                     }
                 ),
+                "scale_input": (
+                    "BOOLEAN",
+                    {
+                        "default": False,
+                    }
+                ),
+                "scale_percentage": (
+                    "FLOAT",
+                    {
+                        "default": 75.0,
+                        "min": 0.1,
+                        "max": 200.0,
+                        "step": 0.1
+                    }
+                ),
+                "enable_model_cpu_offload": (
+                    "BOOLEAN",
+                    {
+                        "default": False,
+                    }
+                ),
+                "enable_sequential_cpu_offload": (
+                    "BOOLEAN",
+                    {
+                        "default": False,
+                    }
+                ),
+                "enable_tiling": (
+                    "BOOLEAN",
+                    {
+                        "default": False,
+                    }
+                ),
+                "enable_slicing": (
+                    "BOOLEAN",
+                    {
+                        "default": False,
+                    }
+                ),
+                "xformers_efficient": (
+                    "BOOLEAN",
+                    {
+                        "default": False,
+                    }
+                ),
             }
         }
 
-    RETURN_TYPES = ("ASYNCDIFF_PIPELINE",)
-    FUNCTION = "launch_host"
+    RETURN_TYPES = ("AD_CONFIG",)
+    FUNCTION = "get_config"
     CATEGORY = "AsyncDiff"        
 
-    def launch_host(self, model, nproc_per_node, model_n, stride, time_shift, variant, scheduler=None):
-        cmd = [
-            'torchrun',
-            f'--nproc_per_node={nproc_per_node}',
-            f'{cwd}/host.py',
-
-            f'--model={checkpoints_dir}/{model}',
-            f'--pipeline_type=svd',
-            f'--model_n={model_n}',
-            f'--stride={stride}',
-            f'--time_shift={time_shift}',
-            f'--variant={variant}',
-            # f'--scheduler={scheduler}',
-        ]
-
-        global pipeline_process
-        pipeline_process = subprocess.Popen(cmd)
-        host = 'http://localhost:6000'
-        while True:
-            try:
-                response = requests.get(f'{host}/initialize')
-                if response.status_code == 200 and response.json().get("status") == "initialized":
-                    print("\n###### Pipeline is ready ######\n")
-                    break
-            except requests.exceptions.RequestException:
-                pass
-            time.sleep(1)
-
-        return (f"{host}/generate", )
+    def get_config(self, model, nproc_per_node, model_n, stride, time_shift, variant, scale_input, scale_percentage,
+        enable_model_cpu_offload, enable_sequential_cpu_offload, enable_tiling, enable_slicing, xformers_efficient
+    ):
+        return (
+            {
+                "model": model,
+                "nproc_per_node": nproc_per_node,
+                "model_n": model_n,
+                "stride": stride,
+                "time_shift": time_shift,
+                "variant": variant,
+                "scale_input": scale_input,
+                "scale_percentage": scale_percentage,
+                "enable_model_cpu_offload": enable_model_cpu_offload,
+                "enable_sequential_cpu_offload": enable_sequential_cpu_offload,
+                "enable_tiling": enable_tiling,
+                "enable_slicing": enable_slicing,
+                "xformers_efficient": xformers_efficient,
+            },
+        )
 
 
-class AsyncDiffImg2VidSampler:
+class ADSVDSampler:
     @classmethod
     def INPUT_TYPES(s):
         return {
             "required": {
-                "pipeline": ("ASYNCDIFF_PIPELINE",),
+                "config": ("AD_CONFIG",),
                 "image": ("IMAGE",),
                 "width": (
                     "INT",
                     {
                         "default": 512,
-                        "min": 0,
-                        "max": 8192,
+                        "min": 8,
+                        "max": 32768,
                         "step": 8
                     }
                 ),
@@ -154,8 +170,8 @@ class AsyncDiffImg2VidSampler:
                     "INT",
                     {
                         "default": 512,
-                        "min": 0,
-                        "max": 8192,
+                        "min": 8,
+                        "max": 32768,
                         "step": 8
                     }
                 ),
@@ -170,7 +186,7 @@ class AsyncDiffImg2VidSampler:
                 "steps": (
                     "INT",
                     {
-                        "default": 30,
+                        "default": 40,
                         "min": 1,
                         "max": 1024
                     }
@@ -208,15 +224,8 @@ class AsyncDiffImg2VidSampler:
                         "step": 0.01
                     }
                 ),
-                "shutdown_pipeline": (
-                    "BOOLEAN",
-                    {
-                        "default": True,
-                    }
-                ),
             }
         }
-
 
     RETURN_TYPES = ("IMAGE",)
     FUNCTION = "generate"
@@ -224,13 +233,11 @@ class AsyncDiffImg2VidSampler:
 
     def generate(
         self,
-        pipeline, image, width, height, steps, seed, decode_chunk_size,
-        num_frames, motion_bucket_id, noise_aug_strength, shutdown_pipeline
+        width, height, steps, seed, decode_chunk_size, num_frames,
+        motion_bucket_id, noise_aug_strength, image, config
     ):
-        global pipeline_process
-        if pipeline_process is None:
-            return (None,)
-        url = pipeline
+        config["pipeline_type"] = "svd"
+        launch_host_process(config)
         image = image.squeeze(0)        # NHWC -> HWC
         image = image.permute(2, 0, 1)  # HWC -> CHW
         image = ToPILImage()(image)
@@ -248,17 +255,14 @@ class AsyncDiffImg2VidSampler:
             "noise_aug_strength": noise_aug_strength,
             "output_path": outputs_dir,
         }
-        response = requests.post(url, json=data)
+        response = requests.post(host_address_generate, json=data)
         response_data = response.json()
         output_base64 = response_data.get("output")
-        if shutdown_pipeline:
-            pipeline_process.terminate()
-            pipeline_process = None
+        close_host_process()
         if output_base64:
             print("Media generated")
             output_bytes = base64.b64decode(output_base64)
             images = pickle.loads(output_bytes)
-            images = images.frames[0]
             tensors = []
             for i in images:
                 tensor = ToTensor()(i)              # CHW
@@ -266,17 +270,189 @@ class AsyncDiffImg2VidSampler:
                 tensors.append(tensor)
             return (torch.stack(tuple(tensors)),)   # HWC -> NHWC
         else:
-            print("No media generated")
-            return (None,)
+            assert False, "No media generated"
+
+
+class ADUpscaleSampler:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "config": ("AD_CONFIG",),
+                "image": ("IMAGE",),
+                "positive_prompt": (
+                    "STRING",
+                    {
+                        "multiline": True,
+                        "default": ""
+                    }
+                ),
+                "negative_prompt": (
+                    "STRING",
+                    {
+                        "multiline": True,
+                        "default": ""
+                    }
+                ),
+                # "scheduler": (
+                #     list([
+                #         "ddim",
+                #         "euler",
+                #         "euler_a",
+                #         "dpm_2",
+                #         "dpm_2_a",
+                #         "dpmpp_2m",
+                #         "dpmpp_2m_sde",
+                #         "dpmpp_sde",
+                #         "heun",
+                #         "lms",
+                #         "pndm",
+                #         "unipc",
+                #     ]),
+                #     {
+                #         "default": "dpmpp_2m",
+                #     }
+                # ),
+                "seed": (
+                    "INT",
+                    {
+                        "default": 0,
+                        "min": 0,
+                        "max": 2 ** 32 - 1
+                    }
+                ),
+                "steps": (
+                    "INT",
+                    {
+                        "default": 60,
+                        "min": 1,
+                        "max": 1024
+                    }
+                ),
+            }
+        }
+
+    RETURN_TYPES = ("IMAGE",)
+    FUNCTION = "generate"
+    CATEGORY = "AsyncDiff"
+
+    def generate(self, positive_prompt, negative_prompt, steps, seed, image, config):
+        config["pipeline_type"] = "sdup"
+        # config["scheduler"] = scheduler
+        if image.size(0) > 1:
+            images = list(torch.unbind(image, 0))   # NHWC -> [HWC], len == N
+        else:
+            images = [image.squeeze(0)]             # NHWC -> [HWC], len == 1
+        tensors = []
+        i = 0
+        launch_host_process(config)
+        for im in images:
+            i += 1
+            im = im.permute(2, 0, 1)                # HWC -> CHW
+            im = ToPILImage()(im)
+            pickled_image = pickle.dumps(im)
+            b64_image = base64.b64encode(pickled_image).decode("utf-8")
+            data = {
+                "positive_prompt": positive_prompt,
+                "negative_prompt": negative_prompt,
+                "image": b64_image,
+                "num_inference_steps": steps,
+                "seed": seed,
+                "output_path": outputs_dir,
+            }
+            print(f"Upscaling image: {i}/{len(images)}")
+            response = requests.post(host_address_generate, json=data)
+            response_data = response.json()
+            output_base64 = response_data.get("output")
+            if output_base64:
+                print(f"Finished upscaling image: {i}/{len(images)}")
+                output_bytes = base64.b64decode(output_base64)
+                im2 = pickle.loads(output_bytes)
+                tensor_image = ToTensor()(im2)                  # CHW
+                tensor_image = tensor_image.permute(1, 2, 0)    # CHW -> HWC
+                tensors.append(tensor_image)
+            else:
+                if len(images) == 1:
+                    assert False, "No media generated"
+                else:
+                    print("Error processing an image.")
+        close_host_process()
+        return (torch.stack(tuple(tensors)),)                   # HWC -> NHWC
+
+
+def launch_host_process(config):
+    global host_process
+
+    if host_process is not None:
+        close_host_process()
+
+    cmd = [
+        'torchrun',
+        f'--nproc_per_node={config.get("nproc_per_node")}',
+        f'{cwd}/host.py',
+
+        '--host_mode=comfyui',
+        f'--model={checkpoints_dir}/{config.get("model")}',
+        f'--pipeline_type={config.get("pipeline_type")}',
+        f'--model_n={config.get("model_n")}',
+        f'--stride={config.get("stride")}',
+        f'--time_shift={config.get("time_shift")}',
+        f'--variant={config.get("variant")}',
+        # f'--scheduler={config.get("scheduler")}',
+    ]
+
+    if config.get("scale_input"):
+        cmd.append('--scale_input')
+        cmd.append(f'--scale_percentage={config.get("scale_percentage")}')
+
+    if config.get("enable_model_cpu_offload"):
+        cmd.append('--enable_model_cpu_offload')
+
+    if config.get("enable_sequential_cpu_offload"):
+        cmd.append('--enable_sequential_cpu_offload')
+
+    if config.get("enable_tiling"):
+        cmd.append('--enable_tiling')
+
+    if config.get("enable_slicing"):
+        cmd.append('--enable_slicing')
+
+    if config.get("xformers_efficient"):
+        cmd.append('--xformers_efficient')
+
+    host_process = subprocess.Popen(cmd)
+    
+    connection_attempts = 0
+    while True:
+        try:
+            response = requests.get(host_address_initialize)
+            if response.status_code == 200 and response.json().get("status") == "initialized":
+                return
+        except requests.exceptions.RequestException:
+            pass
+        time.sleep(1)
+        connection_attempts += 1
+        if connection_attempts >= 60:
+            assert False, "Failed to launch host. Check logs for details."
+
+
+def close_host_process():
+    global host_process
+    if host_process is not None:
+        host_process.terminate()
+        host_process = None
+    return
 
 
 NODE_CLASS_MAPPINGS = {
-    "AsyncDiffImg2VidSampler": AsyncDiffImg2VidSampler,
-    "AsyncDiffSVDPipelineLoader": AsyncDiffSVDPipelineLoader,
+    "ADPipelineConfig": ADPipelineConfig,
+    "ADSVDSampler": ADSVDSampler,
+    "ADUpscaleSampler": ADUpscaleSampler,
 }
 
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "AsyncDiffImg2VidSampler": "AsyncDiffImg2VidSampler",
-    "AsyncDiffSVDPipelineLoader": "AsyncDiffSVDPipelineLoader",
+    "ADPipelineConfig": "ADPipelineConfig",
+    "ADSVDSampler": "ADSVDSampler",
+    "ADUpscaleSampler": "ADUpscaleSampler",
 }
